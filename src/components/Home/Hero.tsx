b66bb722f27f912b359f66e163097bbba30c4509
Edouard Willems <willems.edouard.pro@gmail.com>
2025-04-18 13:13:20 +0200

    style: improve Hero section responsiveness

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -4,34 +4,34 @@ import { GithubIcon } from "../Icons/github";
 import { LinkedinIcon } from "../Icons/linkedIn";
 export default function Hero({ t }: { t: any }) {
   return (
-    <section className="flex flex-col md:flex-row items-center justify-center gap-10 py-20 px-6 max-w-7xl mx-auto">
-      <div className="flex-shrink-0 ">
-        <Image
-          src={ProfilePic}
-          alt={t.hero.alt}
-          className="w-40 h-40 md:w-52 md:h-52 lg:h-72 lg:w-72 rounded-full object-cover shadow-lg"
-        />
-      </div>
-
-      {/* Text content */}
-      <div className="text-center md:text-left">
-        <h1 className="text-6xl font-bold mb-6 leading-[1.2]">
-          {t.hero.title.hi}
-          <br />
+    <section className="flex flex-col lg:flex-row items-center justify-center gap-10 py-20 px-6 max-w-7xl mx-auto ">
+     <div className="w-40 xs:w-72 justify-center   aspect-square relative">
+  <Image
+    src={ProfilePic}
+    alt={t.hero.alt}
+    fill
+    className="rounded-full object-cover shadow-lg"
+  />
+</div>
+      <div className="text-center flex flex-col items-center md:text-left lg:block ">
+        <h1 className="xs:text-6xl text-4xl text-center lg:text-left font-bold mb-6 leading-[1.5] lg:leading-[1.2]">
+          <span className=" sm:pr-2">{t.hero.title.hi}</span>
+          <br className="sm:hidden lg:block"/>
           {t.hero.title.iam}
-          <span className="ml-4 bg-blue-800 text-white px-4 rounded-2xl">
+          <br className="lg:hidden"/>
+          <span className="lg:ml-4  bg-blue-800 text-white px-4 rounded-2xl">
             {t.hero.title.firstname}
           </span>
         </h1>
-        <p className="text-2xl font-semibold text-blue-800 mb-4">
+        <p className="text-2xl font-semibold text-blue-800 mb-4 text-center max-w-sm lg:max-w-screen">
           {t.hero.subtitle}
         </p>
-        <div className="flex justify-center  md:justify-start gap-4">
+        <div className="flex justify-center  lg:justify-start gap-4">
           <a href="https://github.com" aria-label="GitHub">
-            <GithubIcon size={30} />
+            <GithubIcon size={40} />
           </a>
           <a href="https://linkedin.com" aria-label="LinkedIn">
-            <LinkedinIcon size={30} />
+            <LinkedinIcon size={40} />
           </a>
         </div>
       </div>
