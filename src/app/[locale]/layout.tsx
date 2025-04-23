785db80243953aa7d5a9a7c80876f1fdf26314ac
Edouard Willems <willems.edouard.pro@gmail.com>
2025-04-23 14:03:51 +0200

    fix: add missing font weight

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,7 +6,7 @@ const locales = ["fr", "en"];
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["400","500", "600", "700"],
+  weight: ["400", "500", "600", "700"],
   variable: "--font-poppins",
 });
 export default async function LocaleLayout({
